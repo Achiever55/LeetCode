3b78b0a65c44e8382b1a3ad2b61d61ca882f6ba9
Achiever55 <154577175+Achiever55@users.noreply.github.com>
2026-08-19 22:59:00 +0530

    Time: 1 ms (99.94%), Space: 46.9 MB (65.71%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,25 +1,10 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        int max = Integer.MIN_VALUE;
-        int min = Integer.MAX_VALUE;
-        for (int num : nums) {
-            if (num > max) max = num;
-            if (num < min) min = num;
-        }
+     int xor=0;
+     for( int i = 0; i< nums.length; i++){
+        xor = xor^nums[i];
+     }
 
-        int offset = -min;
-        int[] count = new int[max + offset + 1];
-        for (int num : nums) {
-            count[num + offset]++;
-        }
-
-        
-        for (int num : nums) {
-            if (count[num + offset] == 1) {
-                return num;
-            }
-        }
-
-        return -1;
+        return xor;
     }
 }
