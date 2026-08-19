06e76b921dbc2ac34d20d91b02bac40a61bb3f96
Achiever55 <154577175+Achiever55@users.noreply.github.com>
2026-08-19 22:54:18 +0530

    Time: 2 ms (28.7%), Space: 48.2 MB (6.99%) - LeetHub

--- 0136-single-number/0136-single-number.java
+++ 0136-single-number/0136-single-number.java
@@ -1,16 +1,25 @@
 class Solution {
     public int singleNumber(int[] nums) {
-        Set<Integer> set = new HashSet<>();
-        int totalSum = 0;
-        int uniqueSum = 0;
-
+        int max = Integer.MIN_VALUE;
+        int min = Integer.MAX_VALUE;
         for (int num : nums) {
-            if (set.add(num)) {
-                uniqueSum += num;
-            }
-            totalSum += num;
+            if (num > max) max = num;
+            if (num < min) min = num;
         }
 
-        return 2 * uniqueSum - totalSum;
+        int offset = -min;
+        int[] count = new int[max + offset + 1];
+        for (int num : nums) {
+            count[num + offset]++;
+        }
+
+        
+        for (int num : nums) {
+            if (count[num + offset] == 1) {
+                return num;
+            }
+        }
+
+        return -1;
     }
 }
